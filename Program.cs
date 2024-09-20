ee48369c1bb6baa4f683ce14e02faaa6927fbdf3
Vitor Ramos <vitor_rolim11@hotmail.com.br>
2024-09-19 23:07:28 -0300

    Arquivo com a lista de numeros pares

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿List<int> nums = new List<int> {1,2,3,4,5,6,7,8,9,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+
+for (int i = 0; i < nums.Count; i++)
+{
+    if (nums[i] % 2 == 0)
+    {
+        Console.WriteLine($"Número par: {nums[i]}");
+    }
+}
+
+foreach(int numeropar in nums)
+{
+    if (numeropar % 2 == 0)
+    {
+        Console.WriteLine($"Número par: {numeropar}");
+    }
+}
